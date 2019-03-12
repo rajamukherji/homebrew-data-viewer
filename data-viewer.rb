96fd848f54d6c197c2312cfa6cc34cb39849af1a
Raja Mukherji <rajamukherji@gmail.com>
2019-03-12 07:10:48 +0000

    Update data-viewer.rb

--- data-viewer.rb
+++ data-viewer.rb
@@ -2,7 +2,7 @@ class DataViewer < Formula
 	desc "General purpose imperative build system"
 	homepage "https://github.com/rajamukherji/dataviewer"
 	url "https://github.com/rajamukherji/dataviewer.git", :tag => "v1.0.0"
-	depends_on wrapl/rabs/rabs => :build
+	depends_on "wrapl/rabs/rabs" => :build
 	depends_on "pkg-config" => :build
 	depends_on "bdw-gc"
 	depends_on "gtk+3"
