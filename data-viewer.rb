fff4d096e3ddfd46ac530a3dc3bdff940afb6b3d
Raja Mukherji <rajamukherji@gmail.com>
2019-05-09 08:28:42 +0100

    Update data-viewer.rb

--- data-viewer.rb
+++ data-viewer.rb
@@ -1,7 +1,7 @@
 class DataViewer < Formula
 	desc "Viewer for images and associated data"
 	homepage "https://github.com/rajamukherji/data-viewer"
-	url "https://github.com/rajamukherji/data-viewer.git", :tag => "v1.0.4"
+	url "https://github.com/rajamukherji/data-viewer.git", :tag => "v1.0.5"
 	depends_on "wrapl/rabs/rabs" => :build
 	depends_on "pkg-config" => :build
 	depends_on "bdw-gc"
